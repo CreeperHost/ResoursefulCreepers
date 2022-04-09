d1413bb2451d0e1a0f8820473422ee379f0770d9
Gigabit101 <thegigabit101@gmail.com>
2022-04-09 15:40:45 +0100

    Maje it work

--- common/src/main/java/net/creeperhost/resourcefulcreepers/BuildInfo.java
+++ common/src/main/java/net/creeperhost/resourcefulcreepers/BuildInfo.java
@@ -1,13 +1,11 @@
 package net.creeperhost.resourcefulcreepers;
 
-public class BuildInfo
-{
-    public static final String version = "1.0.31";
-    public static final String buildTimestamp = "Sat Jan 15 10:27:26 GMT 2022";
+public class BuildInfo {
+    public static final String version = "e956cd5e0c";
+    public static final String buildTimestamp = "Sat Apr 09 15:40:20 BST 2022";
 
     public String toString() {
         return "version         : " + version + "\n" +
                 "build timestamp : " + buildTimestamp + "\n";
     }
-
 }
