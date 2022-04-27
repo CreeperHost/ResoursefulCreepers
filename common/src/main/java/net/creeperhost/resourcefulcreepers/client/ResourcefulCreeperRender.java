85d2483c131b7b47bb0c872b14d2f7b08e9ae2e1
Gigabit101 <thegigabit101@gmail.com>
2022-04-27 15:11:02 +0100

    Make creepers model swell, fixes #10

--- common/src/main/java/net/creeperhost/resourcefulcreepers/client/ResourcefulCreeperRender.java
+++ common/src/main/java/net/creeperhost/resourcefulcreepers/client/ResourcefulCreeperRender.java
@@ -11,6 +11,7 @@ import net.minecraft.client.renderer.entity.MobRenderer;
 import net.minecraft.client.renderer.entity.RenderLayerParent;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.Mth;
+import net.minecraft.world.entity.monster.Creeper;
 import org.jetbrains.annotations.Nullable;
 
 public class ResourcefulCreeperRender extends MobRenderer<EntityResourcefulCreeper, ResourcefulCreeperModel<EntityResourcefulCreeper>> implements RenderLayerParent<EntityResourcefulCreeper, ResourcefulCreeperModel<EntityResourcefulCreeper>>
@@ -28,6 +29,19 @@ public class ResourcefulCreeperRender extends MobRenderer<EntityResourcefulCreep
     {
         float g = creeper.getSwelling(f);
         return (int)(g * 10.0F) % 2 == 0 ? 0.0F : Mth.clamp(g, 0.5F, 1.0F);
+    }
+
+    @Override
+    public void scale(EntityResourcefulCreeper arg, PoseStack arg2, float f)
+    {
+        float g = arg.getSwelling(f);
+        float h = 1.0F + Mth.sin(g * 100.0F) * g * 0.01F;
+        g = Mth.clamp(g, 0.0F, 1.0F);
+        g *= g;
+        g *= g;
+        float i = (1.0F + g * 0.4F) * h;
+        float j = (1.0F + g * 0.1F) / h;
+        arg2.scale(i, j, i);
     }
 
     @Override
