0021bdaacf0351b682a3c7607dbbb312e30331a5
Gigabit101 <thegigabit101@gmail.com>
2022-04-29 12:49:59 +0100

    Version bump

--- common/src/main/java/net/creeperhost/resourcefulcreepers/BuildInfo.java
+++ common/src/main/java/net/creeperhost/resourcefulcreepers/BuildInfo.java
@@ -1,8 +1,8 @@
 package net.creeperhost.resourcefulcreepers;
 
 public class BuildInfo {
-    public static final String version = "f7bcebf8a9";
-    public static final String buildTimestamp = "Thu Apr 28 13:49:42 BST 2022";
+    public static final String version = "9a8482b719";
+    public static final String buildTimestamp = "Fri Apr 29 12:49:31 BST 2022";
 
     public String toString() {
         return "version         : " + version + "\n" +
