36fbb1aa48c7b10aa372ad9bc6cedf5149150fe4
Gigabit101 <thegigabit101@gmail.com>
2022-11-24 11:20:17 +0000

    Nerfs

--- common/src/main/java/net/creeperhost/resourcefulcreepers/BuildInfo.java
+++ common/src/main/java/net/creeperhost/resourcefulcreepers/BuildInfo.java
@@ -1,8 +1,8 @@
 package net.creeperhost.resourcefulcreepers;
 
 public class BuildInfo {
-    public static final String version = "e5e619d0c5";
-    public static final String buildTimestamp = "Wed May 25 19:55:54 BST 2022";
+    public static final String version = "0c57d91cc5";
+    public static final String buildTimestamp = "Wed Sep 21 17:37:45 BST 2022";
 
     public String toString() {
         return "version         : " + version + "\n" +
