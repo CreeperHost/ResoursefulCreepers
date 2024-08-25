e57edd7b9b8ed390ce4b2e25caec52e5431675ed
brandon3055 <brandon4261@gmail.com>
2024-08-25 14:03:05 +1000

    Fixed broken emerald and diamond spawning.
    They are not supposed to spawn at all.

--- common/src/main/java/net/creeperhost/resourcefulcreepers/BuildInfo.java
+++ common/src/main/java/net/creeperhost/resourcefulcreepers/BuildInfo.java
@@ -1,8 +1,8 @@
 package net.creeperhost.resourcefulcreepers;
 
 public class BuildInfo {
-    public static final String version = "f7d606e829";
-    public static final String buildTimestamp = "Mon Apr 15 12:12:15 AEST 2024";
+    public static final String version = "57ba227bca";
+    public static final String buildTimestamp = "Sun Aug 25 14:01:49 AEST 2024";
 
     public String toString() {
         return "version         : " + version + "\n" +
